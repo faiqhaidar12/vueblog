41c885f59c37f364bd278f14aaa42f1545749d9b
faiqhaidar1 <faiqhaidar1@gmail.com>
2023-06-01 11:48:09 +0700

    setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Unmounted</button>
+    <button @click="posts.pop()">Hapus Post</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from "../components/PostList.vue";
+import { ref } from "vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        id: 1,
+        title: "Artikel Pertama yang saya buat",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      },
+      {
+        id: 2,
+        title: "Artikel Kedua saya loh",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      },
+    ]);
+    const showPosts = ref(true);
+    return { posts, showPosts };
+  },
+};
 </script>
